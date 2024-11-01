8e9e2beab5fa088b3ade5531ab51f37d19fecb07
Nii-Armah <stanleyniiarmah7@gmail.com>
2024-11-01 03:21:22 +0000

    Update HousePriceEstimationUsingFloorAreaView
    It estimates and renders the price of a house given its floor area.

--- pricing/views.py
+++ pricing/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
+from pricing.utils import estimate_house_price_using_floor_area
 
 
 class HomePageView(TemplateView):
@@ -7,6 +10,12 @@ class HomePageView(TemplateView):
 
 class HousePriceEstimationUsingFloorAreaView(TemplateView):
     template_name = 'pricing/floor-area-pricing.html'
+
+    def post(self, request, *args, **kwargs):
+        floor_area = request.POST.get('area')
+        price = estimate_house_price_using_floor_area(floor_area)
+
+        return render(request, self.template_name, {'price': price, 'area': floor_area})
 
 
 class HousePriceEstimationUsingFloorAreaAndTownView(TemplateView):
